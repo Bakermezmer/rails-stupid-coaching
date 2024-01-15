9aaf8fd7c5d2e7e589302a4fd2531b05787005d1
mezmer aboubaker <bakermezmer@gmail.com>
2024-01-15 13:12:02 +0100

    work done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    @question = params[:question]
+    @answer = compute_answer(@question)
+  end
+
+    def compute_answer(question)
+      if question.downcase == 'i am going to work'
+        'Great!'
+      elsif question.end_with?('?')
+        'Silly question, get dressed and go to work!'
+      else
+        "I don't care, get dressed and go to work!"
+      end
+    end
+end
